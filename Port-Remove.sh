eeb1aa05c8d944ea89e89910e1aaa2a906bcb5d2
DanTechBoy <54577461+DanTechBoy@users.noreply.github.com>
2019-09-08 05:40:33 +0400

    Add files via upload

---
+++ Port-Remove.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+echo This will uninstall MacPorts and all of the leftovers from MacPorts.
+
+while true
+ do
+   echo "Are you sure? [Y/n]?"
+    read response
+    case $(echo ${response:-Y}|tr '[:lower:]' '[:upper:]') in
+        Y|YES)
+            sudo port -fp uninstall installed
+            sudo -s rm -rf \
+        /opt/local \
+        /Applications/DarwinPorts \
+        /Applications/MacPorts \
+        /Library/LaunchDaemons/org.macports.* \
+        /Library/Receipts/DarwinPorts*.pkg \
+        /Library/Receipts/MacPorts*.pkg \
+        /Library/StartupItems/DarwinPortsStartup \
+        /Library/Tcl/darwinports1.0 \
+        /Library/Tcl/macports1.0 \
+        ~/.macports
+sudo /usr/bin/dscl . -delete "/Users/macports"
+sudo dscl . -delete "/Groups/macports"
+echo MacPorts has sucessfully been uninstalled from your system. The reason I made this tool is because I really hate MacPorts. If you want a better package manager, try Homebrew.
+            break
+            ;;
+        N|NO)
+            echo "Cancelled."
+            exit
+            ;;
+        *)
+            echo "incorrect selection!"
+            ;;
+    esac
+done
